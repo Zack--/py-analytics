9b7e2c240c778d46c08641814c5a46246f0a90f9
Zack Dibe <zack.dibe@rga.com>
2019-01-10 17:08:58 +0000

    fix: loosen dateutil requirements

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     install_requires=[
         'nydus>=0.10.6',
         'redis>=2.7.2',
-        'python-dateutil==1.5',
+        'python-dateutil>=1.5, !=2.0',
     ],
     tests_require=[
         'nose>=1.0',
